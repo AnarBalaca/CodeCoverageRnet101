fec393dd8599b53899d633d26705c8994ebc2772
unknown <anarbalacayev@gmail.com>
2023-03-10 00:35:44 -0800

    Code Coverage

---
+++ JobApplication.UnitTest/JobApplicationTest.cs
@@ -0,0 +1,28 @@
+﻿using JobApplicationLibrary;
+using JobApplicationLibrary.Models;
+
+namespace JobApplicationUnitTest;
+
+public class JobApplicationTest
+{
+
+
+    [Test]
+    public void ApplicationEvalutor_WithUnderAge_UnderCondition()
+    {
+        // Arrange
+        ApplicationEvaluator evaluator = new ApplicationEvaluator();
+        JobApplication form = new JobApplication()
+        {
+            Applicant = new()
+            {
+                Age = 18
+            }
+        };
+        // Act
+        var result = evaluator.Evaluate(form);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(ApplicationResult.AutoAccepted));
+    }
+}
